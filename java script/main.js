6b7c83228cd7c8a756b0e2bf25865cbfc4de6703
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-04-16 15:20:41 +0200

    first commit

---
+++ java script/main.js
@@ -0,0 +1,11 @@
+// // Dichiaro (con let) una variabile, senza assegnare valore 
+// let miaStringa;
+// // Assegno il valore a una variabile già esistente (senza let)
+// miaStringa = 'Ciao';
+// // utilizzo/richiamo (senza let)
+// alert(miaStringa);
+
+const nome=prompt ("Nome")
+const cognome=prompt ("Cognome")
+const colorePreferito=prompt ("Colore preferito")
+document.getElementById("password").innerHTML = "nome" + "cognome" + "colorePreferito" + "#24"
